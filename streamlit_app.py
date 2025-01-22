dfc2d2e80dda290b85ef28f1c41bd8693b2a4b88
GeethanjaliM30 <84134746+GeethanjaliM30@users.noreply.github.com>
2025-01-22 20:49:22 +0530

    Update streamlit_app.py
    ttt

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ if ingredients_list:
 ###sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 import requests
 smoothiefroot_response = requests.get("https://www.fruityvice.com/#3")
-st.text(smoothiefroot_response.json())
+st.text(smoothiefroot_response.text)
 
 import requests
 smoothiefroot_response = requests.get("https://www.fruityvice.com/#3")
