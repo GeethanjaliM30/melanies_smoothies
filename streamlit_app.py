1c9f4b083e680c00126a901bd3008ab86f06a3ae
GeethanjaliM30 <84134746+GeethanjaliM30@users.noreply.github.com>
2025-01-22 20:45:36 +0530

    Update streamlit_app.py
    tst

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,8 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://www.fruityvice.com/#3")
 #st.text(smoothiefroot_response.json())
-print(smoothiefroot_response.text)  
+#print(smoothiefroot_response.text) 
+st.content(smoothiefroot_response)
 import requests
 smoothiefroot_response = requests.get("https://www.fruityvice.com/#3")
 st.text(smoothiefroot_response)
